9e2569000512a910955f44508127541abdca86b2
atesfay <abrahamkt@yahoo.com>
2019-10-20 23:59:18 -0700

    Fic App Js

--- src/App.js
+++ src/App.js
@@ -2,4 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
 
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <h1>Voyager</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
 
+export default App;
