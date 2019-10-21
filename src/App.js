31c33dae7a72df12adaf187d1802ed5a4ca090fc
atesfay <abrahamkt@yahoo.com>
2019-10-20 23:58:34 -0700

    Apdate App Js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,4 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-      <h1>Voyager</h1>
-      </div>
-    </BrowserRouter>
-  );
-}
 
-export default App;
