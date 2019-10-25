102ca1ba64fc41d4a059e312a4f0ab7c90268856
atesfay <abrahamkt@yahoo.com>
2019-10-25 15:17:55 -0700

    Add Project List and  Project Summary Components

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return(
         <div className="project-list section">
-
-            <div className="crad z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Abraham Tesfay</p>
-                    <p className="grey-text">25 Oct 2019</p>
-                </div>
-            </div>
-
-            <div className="crad z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Abraham Tesfay</p>
-                    <p className="grey-text">25 Oct 2019</p>
-                </div>
-            </div>
-
-            <div className="crad z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Abraham Tesfay</p>
-                    <p className="grey-text">25 Oct 2019</p>
-                </div>
-            </div>
-
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
